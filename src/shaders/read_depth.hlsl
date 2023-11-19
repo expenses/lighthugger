800da235995d38af59d201f46d2ddcce91711e29
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-11-19 20:30:05 +1300

    Move matrices to its own file

--- src/shaders/read_depth.hlsl
+++ src/shaders/read_depth.hlsl
@@ -1,4 +1,5 @@
 #include "bindings.hlsl"
+#include "matrices.hlsl"
 
 [shader("compute")]
 [numthreads(8, 8, 1)]
@@ -27,106 +28,6 @@ void read_depth(uint3 global_id: SV_DispatchThreadID){
         InterlockedMin(depth_info[0].min_depth, subgroup_min);
         InterlockedMax(depth_info[0].max_depth, subgroup_max);
     }
-}
-
-float4x4 InverseRotationTranslation(in float3x3 r, in float3 t)
-{
-    float4x4 inv = float4x4(float4(r._11_21_31, 0.0f),
-                            float4(r._12_22_32, 0.0f),
-                            float4(r._13_23_33, 0.0f),
-                            float4(0.0f, 0.0f, 0.0f, 1.0f));
-    inv[3][0] = -dot(t, r[0]);
-    inv[3][1] = -dot(t, r[1]);
-    inv[3][2] = -dot(t, r[2]);
-    return inv;
-}
-
-
-// Taken from https://www.geertarien.com/blog/2017/07/30/breakdown-of-the-lookAt-function-in-OpenGL/
-float4x4 lookAt(float3 eye, float3 at, float3 up)
-{
-  float3 zaxis = -normalize(at - eye);
-  float3 xaxis = normalize(cross(zaxis, up));
-  float3 yaxis = cross(xaxis, zaxis);
-
-  return float4x4(
-    float4(xaxis.x, xaxis.y, xaxis.z, -dot(xaxis, eye)),
-    float4(yaxis.x, yaxis.y, yaxis.z, -dot(yaxis, eye)),
-    float4(zaxis.x, zaxis.y, zaxis.z, -dot(zaxis, eye)),
-    float4(0, 0, 0, 1)
-  );
-}
-
-
-float4x4 OrthographicProjection(float l, float b, float r,
-                                float t, float zn, float zf)
-{
-    //return float4x4(float4(2.0f / (r - l), 0, 0, 0),
-    //                float4(0, 2.0f / (t - b), 0, 0),
-    //                float4(0, 0, 1 / (zf - zn), 0),
-    //                float4((l + r) / (l - r), (t + b)/(b - t), zn / (zn - zf),  1));
-    float right_plane = r;
-    float left_plane = l;
-    float bottom_plane = b;
-    float top_plane = t;
-    float near_plane = zn;
-    float far_plane = zf;
-    return transpose(float4x4(
-              2.0f / (right_plane - left_plane),
-      0.0f,
-      0.0f,
-      0.0f,
-
-
-      0.0f,
-      2.0f / (bottom_plane - top_plane),
-      0.0f,
-      0.0f,
-
-
-      0.0f,
-      0.0f,
-      1.0f / (near_plane - far_plane),
-      0.0f,
-
-
-      -(right_plane + left_plane) / (right_plane - left_plane),
-      -(bottom_plane + top_plane) / (bottom_plane - top_plane),
-      near_plane / (near_plane - far_plane),
-      1.0f
-    ));
-}
-
-float4 PlaneFromPoints(in float3 point1, in float3 point2, in float3 point3)
-{
-    float3 v21 = point1 - point2;
-    float3 v31 = point1 - point3;
-
-
-    float3 n = normalize(cross(v21, v31));
-    float d = -dot(n, point1);
-
-
-    return float4(n, d);
-}
-
-float4x4 InverseScaleTranslation(in float4x4 m)
-{
-    float4x4 inv = float4x4(float4(1.0f, 0.0f, 0.0f, 0.0f),
-                            float4(0.0f, 1.0f, 0.0f, 0.0f),
-                            float4(0.0f, 0.0f, 1.0f, 0.0f),
-                            float4(0.0f, 0.0f, 0.0f, 1.0f));
-
-
-    inv[0][0] = 1.0f / m[0][0];
-    inv[1][1] = 1.0f / m[1][1];
-    inv[2][2] = 1.0f / m[2][2];
-    inv[3][0] = -m[3][0] * inv[0][0];
-    inv[3][1] = -m[3][1] * inv[1][1];
-    inv[3][2] = -m[3][2] * inv[2][2];
-
-
-    return inv;
 }
 
 [shader("compute")]
@@ -170,8 +71,6 @@ void generate_matrices(uint3 global_id: SV_DispatchThreadID)
             cascadeSplits[i] = (d - nearClip) / clipRange;
     }
 
-    // Calculate orthographic projection matrix for each cascade
-    // 1.0 - because we're using reverse z.
     float lastSplitDist = min_depth;//(cascade_index > 0 ? cascadeSplits[cascade_index - 1] : min_depth);
     float splitDist = max_depth;//cascadeSplits[cascade_index];
 
--- src/shaders/render_geometry.hlsl
+++ src/shaders/render_geometry.hlsl
@@ -1,6 +1,10 @@
 #include "inputs/pos.hlsl"
 #include "bindings.hlsl"
 
+uint load_index(uint64_t address, uint vertex_id) {
+    return vk::RawBufferLoad<uint>(address + sizeof(uint) * vertex_id);
+}
+
 float3 load_float3(uint64_t address, uint offset) {
     return float3(
         vk::RawBufferLoad<float>(address + sizeof(float) * (offset * 3)),
@@ -16,7 +20,7 @@ float4 depth_only(
 {
     MeshBufferAddresses addresses = mesh_buffer_addresses.Load(0);
 
-    uint offset = vk::RawBufferLoad<uint>(addresses.indices + sizeof(uint) * vId);
+    uint offset = load_index(addresses.indices, vId);
     float3 position = load_float3(addresses.positions, offset);
 
     return mul(uniforms.combined_perspective_view, float4(position, 1.0));
@@ -29,7 +33,7 @@ float4 shadow_pass(
 {
     MeshBufferAddresses addresses = mesh_buffer_addresses.Load(0);
 
-    uint offset = vk::RawBufferLoad<uint>(addresses.indices + sizeof(uint) * vId);
+    uint offset = load_index(addresses.indices, vId);
     float3 position = load_float3(addresses.positions, offset);
 
     return mul(depth_info[0].shadow_rendering_matrices[0], float4(position, 1.0));
@@ -41,7 +45,7 @@ V2P VSMain(uint vId : SV_VertexID)
     MeshBufferAddresses addresses = mesh_buffer_addresses.Load(0);
 
     uint material_index = vk::RawBufferLoad<uint>(addresses.material_indices + sizeof(uint) * vId);
-    uint offset = vk::RawBufferLoad<uint>(addresses.indices + sizeof(uint) * vId);
+    uint offset = load_index(addresses.indices, vId);
     float3 position = load_float3(addresses.positions, offset);
     float3 normal = load_float3(addresses.normals, offset);
     float2 uv = vk::RawBufferLoad<float2>(addresses.uvs + sizeof(float2) * offset);
@@ -61,7 +65,6 @@ float textureProj(float4 shadowCoord, float2 offset, uint cascadeIndex)
     float shadow = 1.0;
     float bias = 0.005;
 
-
     if ( shadowCoord.z > -1.0 && shadowCoord.z < 1.0 ) {
             float dist = shadowmap.Sample(clamp_sampler, float2(shadowCoord.xy + offset));
             if (shadowCoord.w > 0 && dist < shadowCoord.z - bias) {
@@ -72,10 +75,10 @@ float textureProj(float4 shadowCoord, float2 offset, uint cascadeIndex)
 }
 
 static const float4x4 biasMat = float4x4(
-        0.5, 0.0, 0.0, 0.5,
-        0.0, 0.5, 0.0, 0.5,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
+    0.5, 0.0, 0.0, 0.5,
+    0.0, 0.5, 0.0, 0.5,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0
 );
 
 [shader("pixel")]
