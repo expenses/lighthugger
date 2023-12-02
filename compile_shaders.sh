1a968a8d448be2084b32abdf75653a34e195d9a5
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-12-02 21:04:53 +1300

    Add -Werror

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,4 +1,4 @@
-glsl_flags="-O -DGLSL=1 -std=450 --target-env=vulkan1.3 --target-spv=spv1.6 -I src"
+glsl_flags="-Werror -O -DGLSL=1 -std=450 --target-env=vulkan1.3 --target-spv=spv1.6 -I src"
 
 python compile_glsl.py --out-dir compiled_shaders --flags "$glsl_flags" src/shaders/*.{glsl,comp} || exit 1
 python compile_glsl.py --out-dir compiled_shaders/compute --flags "$glsl_flags" --shader-stage=comp src/shaders/compute/*.* || exit 1
